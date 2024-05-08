88019a6c827241a2980f340e0bde21ef3b509903
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-05-08 18:04:09 +0300

    adaugat cors, adaugat un get pt produse

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const database = require('./config/database');
-const eventRoutes = require('./routes/eventRoutes');
+const express = require("express");
+const database = require("./config/database");
+const eventRoutes = require("./routes/eventRoutes");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use('/events', eventRoutes);
+app.use(cors());
+app.use("/events", eventRoutes);
 
 const port = 3001;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
 
+app.get("/", (req, res) => {
+  res.send("PAGINA DE BACKEND!");
+});
+
+app.get("/api/products", (req, res) => {
+  const placeHolderImage = "../../../public/item.png";
+  const products = [
+    { name: "Bec", image: placeHolderImage },
+    { name: "Lingura", image: placeHolderImage },
+    { name: "Hartie", image: placeHolderImage },
+    { name: "Stilou", image: placeHolderImage },
+    { name: "Pix", image: placeHolderImage },
+    { name: "Coca-Cola", image: placeHolderImage },
+    { name: "Pepsi", image: placeHolderImage },
+    { name: "Ciuperca", image: placeHolderImage },
+    { name: "Priza", image: placeHolderImage },
+  ];
+  res.json(products);
+});
