831f363047eeabc7f18cd82443eb5588143452c8
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-05-14 22:33:19 +0300

    adaugat axios, adaugat niste post request-uri pentru incarcare produs, descarcare imagine produs in folderu din backend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ const database = require("./config/database");
 const eventRoutes = require("./routes/eventRoutes");
 const cors = require("cors");
 const Product = require("./models/productModel");
+const fs = require("fs");
+const path = require("path");
+const { error } = require("console");
+const axios = require("axios");
+
+const product_images_folder = "./product_images";
 
 const app = express();
 app.use(express.json());
@@ -28,4 +34,53 @@ app.post("/api/add_product", async (req, res) => {
   const productToAdd = new Product(JSON.parse(req.body.product));
   await productToAdd.save();
   res.send("Backendul a primit post request pt adaugare obiect!");
+});
+
+app.post("/api/get_product_image", async (req, res) => {
+  const productDetails = JSON.parse(req.body.productDetails);
+  if (productDetails.seller != undefined) {
+    let imagePath = path.join(
+      product_images_folder,
+      productDetails.name + "_" + productDetails.seller + ".png"
+    );
+
+    fs.readFile(imagePath, (error, data) => {
+      if (error) console.log(error);
+      else {
+        let mime = "image/png";
+        let encoding = "base64";
+        let uri =
+          "data:" + mime + ";" + encoding + "," + data.toString(encoding);
+        res.json({ ok: true, img: uri });
+      }
+    });
+  } else
+    res.json({
+      ok: false,
+      error: "INVALID PRODUCT, NO SELLER : " + productDetails.name,
+    });
+});
+
+app.post("/api/download_product_image", async (req, res) => {
+  const imageUrl = req.body.img_src;
+  let imagePath = path.join(
+    product_images_folder,
+    req.body.name + "_" + req.body.seller + ".png"
+  );
+
+  const file = fs.createWriteStream(imagePath);
+  axios({
+    url: imageUrl,
+    responseType: "stream",
+  }).then((response) => {
+    response.data.pipe(file); //pune in fisier informatia binara
+    file.on("finish", () => {
+      file.close();
+      res.send("Descarcat imaginea produsului cu succes!");
+    });
+    file.on("error", (err) => {
+      fs.unlink(imagePath);
+      res.send("Descarcarea imaginii produsului a esuat");
+    });
+  });
 });
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -17,6 +17,10 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  price: {
+    type: Number,
+    required: true,
+  },
   num_reviews: {
     type: Number,
     required: false,
