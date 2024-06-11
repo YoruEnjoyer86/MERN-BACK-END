a3a3fb556096791f677fd03d88f7be6eca12f798
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-06-11 23:06:16 +0300

    schimbat setari cors, adaugat inca un middleware

--- src/index.js
+++ src/index.js
@@ -22,37 +22,38 @@ const local_front_url = "http://localhost:5173";
 const deployed_front_url = "https://ecommerce-bibart-alexandru.onrender.com";
 
 const app = express();
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? deployed_front_url
-        : local_front_url,
-    methods: [
-      "GET",
-      "HEAD",
-      "PUT",
-      "PATCH",
-      "POST",
-      "DELETE",
-      "OPTIONS",
-      "CONNECT",
-      "TRACE",
-    ],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Content-Type-Options",
-      "Accept",
-      "X-Requested-With",
-      "Origin",
-      "Access-Control-Request-Method",
-      "Access-Control-Request-Headers",
-    ],
-    credentials: true,
-    maxAge: 7200,
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://your-frontend.com");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
+app.options("*", (req, res) => {
+  // console.log("preflight");
+  if (
+    req.headers.origin === deployed_front_url &&
+    allowMethods.includes(req.headers["access-control-request-method"]) &&
+    allowHeaders.includes(req.headers["access-control-request-headers"])
+  ) {
+    console.log("pass");
+    return res.status(204).send();
+  } else {
+    console.log("fail");
+  }
+});
+
 app.use(express.json());
 
 //app.use("/events", eventRoutes);
